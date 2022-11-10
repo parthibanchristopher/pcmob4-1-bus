2cd0255abfff537e1472f346361ef5b1ec90616d
parthibanchristopher <parthibanchristopher@gmail.com>
2022-11-10 21:03:20 +0800

    basic layout

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";		
+import { ActivityIndicator, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View style={styles.container}>		
+      <Text style={styles.title}>Bus arrival time:</Text>		
+      <Text style={styles.arrivalTime}>
+          {loading ? "loading" : "loaded"}
+      </Text>		
+      <TouchableOpacity style={styles.button} onPress={() => setLoading(false)}>		
+        <Text style={styles.buttonText}>Refresh</Text>		
+      </TouchableOpacity>		
+    </View>		
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({		
+  container: {		
+    flex: 1,		
+    backgroundColor: "#fff",		
+    alignItems: "center",		
+    justifyContent: "center",		
+  },		
+  title: {		
+    fontSize: 50,		
+    marginVertical: 20,		
+  },		
+  arrivalTime: {		
+    fontSize: 40,		
+  },		
+  button: {		
+    backgroundColor: "lightblue",		
+    padding: 20,		
+    marginVertical: 20,		
+  },		
+  buttonText: { fontSize: 20 },		
+});		
